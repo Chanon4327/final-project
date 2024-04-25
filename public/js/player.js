6d1853204852d7a913bb8963619c4eb8134e1eca
cmk224 <cmk224@lehigh.edu>
2024-04-25 14:48:21 -0400

    add charge jumping functionality for the player

--- public/js/player.js
+++ public/js/player.js
@@ -1,20 +1,63 @@
 /** governs the player's properties and behaviors, movement, jumping */
 
+import { canvas, ctx } from './script.js'
+
 export class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.width = 30;
-        this.height = 30;
+    constructor() {
+        this.x = canvas.width / 2;
+        this.y = canvas.height - 20; // minus the width
+        this.width = 20;
+        this.height = 20;
         this.speed = 5;
-        this.jumpPower = 20;
+        this.maxJumpPower = 40;
+        this.minJumpPower = 10;
+        this.jumpCharge = 0; // how much the jump is charged
         this.gravity = 2;
         this.velocityY = 0;
+        this.isJumping = false;
+    }
+
+    startJump() {
+        if (this.onGround() && !this.isJumping) {
+            this.isJumping = true;
+            this.jumpCharge = this.minJumpPower;
+        }
+    }
+
+    endJump() {
+        if (this.isJumping) {
+            this.isJumping = false;
+            this.velocityY = -this.jumpCharge;
+            this.jumpCharge = 0;
+            console.log('Jump ended with charge:', this.velocityY);
+        }
+    }
+
+    chargeJump() {
+        if (this.isJumping && this.jumpCharge < this.maxJumpPower) {
+            this.jumpCharge += 1;
+            //console.log (this.jumpCharge)
+        } 
+    }
+
+    // will need to change this to account for platform as well
+    onGround() {
+        return this.y >= canvas.height - this.height;
     }
 
     update() {
+        this.chargeJump();
+        console.log('Updating, jumpCharge:', this.jumpCharge);
+        
         this.velocityY += this.gravity;
         this.y += this.velocityY;
+
+        // prevent from dropping through ground
+        if (this.y > canvas.height - this.height) {
+            this.y = canvas.height - this.height;
+            this.velocityY = 0;
+        }
+
     }
 
     draw(ctx) {
--- public/js/script.js
+++ public/js/script.js
@@ -4,12 +4,12 @@
 
 import { Player } from './player.js'
 
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+export const canvas = document.getElementById('game-canvas');
+export const ctx = canvas.getContext('2d');
 canvas.width = 800; // change according to the canvas width
 canvas.height = 600; // change according to the canvas height
 
-let player = new Player(100, 100); // postion the player
+let player = new Player(); // create player object
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -17,5 +17,17 @@ function gameLoop() {
     player.draw(ctx);
     requestAnimationFrame(gameLoop);
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        player.startJump();
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.code === 'Space') {
+        player.endJump();
+    }
+});
 
 gameLoop();
